54dbd7c96ad8192f2f317e8e79bb055874256017
Luiz Eduardo da Silva <luizedsilva@gmail.com>
2026-09-21 23:01:45 -0300

    Correção - Capitulo 5

--- chapters/images/qrcode/gen-qrcode.sh
+++ chapters/images/qrcode/gen-qrcode.sh
@@ -5,4 +5,5 @@ BASE_URL="${PDI_BOOK_URL:-https://luizedsilva.github.io/Livro-LTC}"
 qrencode -o syntaxTree.png "${BASE_URL}/p5js/syntaxTree/"
 qrencode -o er-af.png "${BASE_URL}/p5js/er-af/"
 qrencode -o lr.png "${BASE_URL}/p5js/lr/"
+qrencode -o cyk.png "${BASE_URL}/p5js/cyk/"
 
